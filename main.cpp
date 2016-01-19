01a966e1ed4076673c18071e99610fc4ed19683d
Ivo Georgiev <ivogeorg@gmail.com>
2016-01-18 21:43:50 -0700

    Added a simple main file.

---
+++ main.cpp
@@ -0,0 +1,14 @@
+/**
+ * File: main.cpp
+ * Project: ucd-csci2312-pa0
+ * Author: Ivo Georgiev
+ * Date: 2016-01-18
+ */
+#include <iostream>
+
+int main() {
+    // TODO Substitute 11 with your UCD ID number, modulo 13 (myId % 13)
+    std::cout << "Hello, world! My number is " << 11 << '!' << std::endl;
+
+    return 0;
+}
