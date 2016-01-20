f2849e9d7599a81a3f2a1aea9357be8061e76f3f
Ivo Georgiev <ivogeorg@gmail.com>
2016-01-20 14:43:26 -0700

    Trivial change.

--- main.cpp
+++ main.cpp
@@ -8,7 +8,7 @@
 
 int main() {
     // TODO Substitute 11 with your UCD ID number, modulo 13 (myId % 13)
-    std::cout << "Hello, world! My number is " << 11 << '!' << std::endl;
+    std::cout << "Hello, world! My number is " << 10 << '!' << std::endl;
 
     return 0;
 }
